591126d83eb95a00a12c6ecead1d4d6e875aafe2
ampregnall <ampregnall@gmail.com>
2026-03-27 11:03:32 -0400

    feat: add LDSC correction

---
+++ bin/ldsc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import gwaslab as gl
+import numpy as np
+from scipy.stats import norm
+
+parser = argparse.ArgumentParser(description="Test gwaslab installation")
+parser.add_argument("--input", type=str, required=True, help="Path to input file")
+parser.add_argument("--output", type=str, required=True, help="Output filenmame")
+parser.add_argument("--ldsc", type=str, required=True, help = "Path to Pan-UKBB LD reference panels")
+args = parser.parse_args()
+
+
+sumstats = gl.Sumstats(args.input, fmt="gwaslab", build="38")
+
+# Perform LDSC correction
+sumstats_hapmap3 = sumstats.filter_hapmap3(inplace=False)
+sumstats_hapmap3.estimate_h2_by_ldsc(ref_ld = args.ldsc,  w_ld = args.ldsc)
+
+
+if np.float64(sumstats_hapmap3.ldsc_h2['Intercept'][0]) > 1:
+    sumstats.data['SE'] = sumstats.data['SE'] * np.sqrt(np.float64(sumstats_hapmap3.ldsc_h2['Intercept'][0]))
+    sumstats.data['Z'] = sumstats.data['BETA'] / sumstats.data['SE']
+    sumstats.data['P'] = 2 * norm.sf(abs(sumstats.data['Z']))
+    
+# Save results
+sumstats_out = f"{args.output}.sumstats.processed.txt.gz"
+parquet_out = f"{args.output}.sumstats.parquet"
+sumstats.data.to_csv(sumstats_out, index = False, compression="gzip", sep = "\t")   
---
+++ modules/ldsc_correction.nf
@@ -0,0 +1,24 @@
+process LDSC_CORRECTION {
+    cpus 1
+    container 'ghcr.io/ampregnall/nf-meta-gwas:0.2.0'
+    publishDir { "${launchDir}/data/sumstats-processed/${meta.phenotype}" }, mode: 'copy'
+    memory { 16.GB + (12.GB * (task.attempt - 1)) }
+    maxRetries 4
+    errorStrategy { task.exitStatus in 137..140 ? 'retry' : 'finish' }
+    
+    input:
+        tuple val(meta), path(sumstats)
+
+    output:
+        tuple val(meta), path("*.sumstats.processed.txt.gz"), emit: sumstats_munged
+
+    script:
+    def prefix = "${meta.phenotype}-${meta.cohort}-${meta.population}"
+    def ldsc = params.ldsc_reference[meta.population]
+    """
+    ldsc.py \
+      --input ${sumstats} \
+      --output ${prefix} \
+      --ldsc ${ldsc}
+    """
+}
