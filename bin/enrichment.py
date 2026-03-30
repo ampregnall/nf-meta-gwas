cced8ebcb7f6b6e8e2c5f0956e08398a20bccb2e
ampregnall <ampregnall@gmail.com>
2026-03-30 16:07:47 -0400

    fix: update parameter pathways and change argparse names to avoid confusion with LD reference panels

--- bin/enrichment.py
+++ bin/enrichment.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Tissue and cell type enrichment analysis using LDSC-CTS")
 parser.add_argument("--input",   type=str, required=True, help="Path to input file")
 parser.add_argument("--output",  type=str, required=True, help="Output filename")
-parser.add_argument("--ld",      type=str, required=True, help="Path to Pan-UKBB LD reference panels")
+parser.add_argument("--ldbaseline",      type=str, required=True, help="Path to baseline LD files")
 parser.add_argument("--ldcts",   type=str, required=True, help="Path to cell-type LD scores file")
 parser.add_argument("--weights", type=str, required=True, help="Path to LD score weights")
 args = parser.parse_args()
